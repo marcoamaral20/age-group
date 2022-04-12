d6515eb641def4ddb1547ac5685921bea16b1ad4
Marco Amaral <marcoantonio.123@hotmail.com>
2022-04-11 23:01:26 -0300

    create header select age group

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("TE AMO MOR")
+document.querySelectorAll('.select-group-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const content = button.nextElementSibling
+
+        button.classList.toggle('select-group-button--active');
+
+        if (button.classList.contains('select-group-button--active')) {
+            content.style.maxHeight = content.scrollHeight + 'px';
+        } else {
+            content.style.maxHeight = 0;
+        }
+    })
+})
